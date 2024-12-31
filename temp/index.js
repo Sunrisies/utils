a04e79c09089ad0dea06a7dfefc1ec9bcf5e781e
朱重迁 <3266420686@qq.com>
2024-12-31 10:29:22 +0800

    特性: 🚀 添加由一个经纬度获取四个点的经纬度

---
+++ temp/index.js
@@ -0,0 +1,4 @@
+import { getContentDimensions, preloadAndCacheImages, conversionTime, genRandStr, preloadAndCacheImage } from './utils/index';
+import { TimeUpdater } from './utils/timer';
+import { distanceLngLat, calculateNewPoints } from './utils/map';
+export { getContentDimensions, preloadAndCacheImages, conversionTime, genRandStr, TimeUpdater, preloadAndCacheImage, distanceLngLat, calculateNewPoints };
--- temp/utils/index.js
+++ temp/utils/index.js
@@ -3,7 +3,7 @@
  *
  * @param {string} elementId - 要获取尺寸的元素的ID。
  * @returns {Error | { width: number, height: number }} - 返回一个包含width和height的对象，如果找不到元素或无法获取计算样式则返回null。
- * 
+ *
  * @example
  * // 示例：获取ID为 'myElement' 的元素的内容区域尺寸
  * const dimensions = getContentDimensions('myElement')
@@ -14,38 +14,37 @@
  *   console.error('无法获取元素的尺寸')
  * }
  */
-export const getContentDimensions = (elementId: string): Error | { width: number, height: number } => {
-  const element = document.getElementById(elementId)
-  if (element) {
-    const rect = element.getBoundingClientRect()
-    const style = window.getComputedStyle(element)
-    if (style) {
-      const paddingTop = parseFloat(style.paddingTop)
-      const paddingRight = parseFloat(style.paddingRight)
-      const paddingBottom = parseFloat(style.paddingBottom)
-      const paddingLeft = parseFloat(style.paddingLeft)
-      const contentWidth = rect.width - (paddingLeft + paddingRight)
-      const contentHeight = rect.height - (paddingTop + paddingBottom)
-
-      return {
-        width: contentWidth,
-        height: contentHeight
-      }
-    } else {
-      return new Error('Element has no computed style' + elementId)
+export const getContentDimensions = (elementId) => {
+    const element = document.getElementById(elementId);
+    if (element) {
+        const rect = element.getBoundingClientRect();
+        const style = window.getComputedStyle(element);
+        if (style) {
+            const paddingTop = parseFloat(style.paddingTop);
+            const paddingRight = parseFloat(style.paddingRight);
+            const paddingBottom = parseFloat(style.paddingBottom);
+            const paddingLeft = parseFloat(style.paddingLeft);
+            const contentWidth = rect.width - (paddingLeft + paddingRight);
+            const contentHeight = rect.height - (paddingTop + paddingBottom);
+            return {
+                width: contentWidth,
+                height: contentHeight
+            };
+        }
+        else {
+            return new Error('Element has no computed style' + elementId);
+        }
     }
-  } else {
-    return new Error('Element not found with ID' + elementId)
-  }
-}
-
-
+    else {
+        return new Error('Element not found with ID' + elementId);
+    }
+};
 /**
  * 预加载并缓存一组图片资源。
- * 
+ *
  * @param {string[]} imageUrls - 图片资源的URL数组。
  * @returns {Promise<HTMLImageElement[]>} - 返回一个Promise，该Promise在所有图片加载完成后解析为一个包含HTMLImageElement对象的数组。
- * 
+ *
  * @example
  * // 示例：预加载并缓存一组图片
  * preloadAndCacheImages(['https://example.com/image1.jpg', 'https://example.com/image2.jpg'])
@@ -56,20 +55,14 @@ export const getContentDimensions = (elementId: string): Error | { width: number
  *     console.error('图片加载失败', error);
  *   });
  */
-export const preloadAndCacheImages = (imageUrls: string[]): Promise<HTMLImageElement[]> => {
-  return Promise.all(
-    imageUrls.map(
-      (url) =>
-        new Promise<HTMLImageElement>((resolve, reject) => {
-          const image = new Image();
-          image.onload = () => resolve(image);
-          image.onerror = () => reject(new Error(`图片加载失败: ${url}`));
-          image.src = url;
-        })
-    )
-  );
+export const preloadAndCacheImages = (imageUrls) => {
+    return Promise.all(imageUrls.map((url) => new Promise((resolve, reject) => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.onerror = () => reject(new Error(`图片加载失败: ${url}`));
+        image.src = url;
+    })));
 };
-
 /**
  * 预加载并缓存图片资源。
  *
@@ -86,19 +79,18 @@ export const preloadAndCacheImages = (imageUrls: string[]): Promise<HTMLImageEle
  *     console.error('图片加载失败', error);
  *   });
  */
-export const preloadAndCacheImage = (imageUrl: string): Promise<HTMLImageElement> => {
-  return new Promise((resolve, reject) => {
-    const img = new Image()
-    img.onload = () => {
-      resolve(img)
-    }
-    img.onerror = () => {
-      reject(new Error('图片加载失败'))
-    }
-    img.src = imageUrl
-  })
-}
-
+export const preloadAndCacheImage = (imageUrl) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => {
+            resolve(img);
+        };
+        img.onerror = () => {
+            reject(new Error('图片加载失败'));
+        };
+        img.src = imageUrl;
+    });
+};
 /**
  * 时间戳转换为日期字符串。
  *
@@ -110,16 +102,15 @@ export const preloadAndCacheImage = (imageUrl: string): Promise<HTMLImageElement
  * const dateString = conversionTime(1633072800)
  * console.log(dateString) // 输出类似 '2021年10月1日00时00分'
  */
-export const conversionTime = (time: number): string => {
-  const date = new Date(time * 1000)
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hours = date.getHours()
-  const minutes = date.getMinutes()
-  return `${year}年${month}月${day}日${hours}时${minutes}分`
-}
-
+export const conversionTime = (time) => {
+    const date = new Date(time * 1000);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    return `${year}年${month}月${day}日${hours}时${minutes}分`;
+};
 /**
  * 随机生成指定长度的字符串。
  *
@@ -142,16 +133,15 @@ export const conversionTime = (time: number): string => {
  *   console.error(invalidStr.message) // 输出 'Length must be a positive number'
  * }
  */
-export const genRandStr = (length: number): string | Error => {
-  if (typeof length !== 'number' || length <= 0) {
-    return new Error('Length must be a positive number')
-  }
-
-  const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-  let randomString: string = ''
-  for (let i = 0; i < length; i++) {
-    const randomIndex: number = Math.floor(Math.random() * characters.length)
-    randomString += characters.charAt(randomIndex)
-  }
-  return randomString
-}
+export const genRandStr = (length) => {
+    if (typeof length !== 'number' || length <= 0) {
+        return new Error('Length must be a positive number');
+    }
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        randomString += characters.charAt(randomIndex);
+    }
+    return randomString;
+};
