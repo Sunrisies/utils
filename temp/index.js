6fc7ad23018417344aec1187440970b7d1e53c0f
朱重迁 <z32664206862022@163.com>
2025-01-09 09:03:23 +0800

    文档: 📚 删除不必要的文件

--- temp/index.js
+++
@@ -1,4 +0,0 @@
-import { getContentDimensions, preloadAndCacheImages, conversionTime, genRandStr, preloadAndCacheImage } from './utils/index';
-import { TimeUpdater } from './utils/timer';
-import { distanceLngLat, calculateNewPoints } from './utils/map';
-export { getContentDimensions, preloadAndCacheImages, conversionTime, genRandStr, TimeUpdater, preloadAndCacheImage, distanceLngLat, calculateNewPoints, };
